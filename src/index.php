c689bb29cca8243e36e73d171deaa478ffb389f2
GianAle1 <115366017+GianAle1@users.noreply.github.com>
2024-12-09 10:54:20 -0500

    .

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="Perro, Perdido,Busqueda">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    
+
     <link href="style.css" rel="stylesheet">
     <title>Mascotas Perdidas</title>
     <link rel="icon" type="image/x-icon" href="views/image/fivicon.jpeg">
